f4fa0a30fde98edb752dd45999ef0bfc289d281a
nishimwe-tharcilla <nishimwetharcilla43@gmail.com>
2025-04-13 12:56:15 +0200

    Add files via upload

---
+++ tharcilla quiz.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?$sentence="The main body of a Book";
+    echo strtolower($sentence);
+    echo strtoupper($sentence);
+    echo strlen($sentence);
+    $sentence[0]="m";
+    ?>
+    </body>
+</html>
